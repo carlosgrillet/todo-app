2929b31d50a2f2cf6816b37480b14836c906342c
Carlos Grillet <carlosbeta5000@gmail.com>
2023-03-11 11:09:50 -0400

    renderTodos function added as use-case

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,24 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './models/use-cases';
+
+const elementIds = {
+  TodoList: '.todo-list',
+};
+
 /**
  *
  * @param {String} elementId Pagina web
  */
 export const App = (elementId) => {
+  const generarTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(elementIds.TodoList, todos);
+  };
   (() => {
     const app = document.createElement('div');
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    generarTodos();
   })();
 };
